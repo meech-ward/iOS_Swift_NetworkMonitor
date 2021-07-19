16f2303903894601cb447e90207566d87446fb19
Sam Meech-Ward <sam@meech-ward.com>
2021-07-19 10:28:00 -0700

    Initial Commit

---
+++ NetworkDemo/NetworkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDemoApp.swift
+//  NetworkDemo
+//
+//  Created by Sam Meech-Ward on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
